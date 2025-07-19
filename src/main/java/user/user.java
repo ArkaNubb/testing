ea74f60750032a427c77fbdde351f344b468aeb7
Sabbir <sabbirbuetdc@gmail.com>
2025-07-19 12:34:05 +0600

    loadingfileinformation

--- src/main/java/user/user.java
+++ src/main/java/user/user.java
@@ -1,16 +1,17 @@
 package user;
 
 public abstract class user {
-    protected String userId;
-    protected String name;
     protected String email;
+    protected String userId;
     protected String password;
+    protected String name;
 
-    public user(String email, String userId, String name, String password) {
+    public user(String email, String userId, String password, String name) {
         this.email = email;
         this.userId = userId;
-        this.name = name;
         this.password = password;
+        this.name = name;
+
     }
 // I am Pritom
     abstract public void printInfo();
